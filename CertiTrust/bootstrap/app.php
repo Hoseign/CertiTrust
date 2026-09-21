70e9048ed3ed50210faf5adcdc85388dc8d9680f
Hoseign. <randygonzales2024@gmail.com>
2026-09-21 21:56:46 +0800

    Return JSON errors for API authentication

--- CertiTrust/bootstrap/app.php
+++ CertiTrust/bootstrap/app.php
@@ -16,5 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function ($request, $exception) {
+            return $request->is('api/*') || $request->expectsJson();
+        });
     })->create();
